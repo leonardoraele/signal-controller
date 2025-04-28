ae7585eab45bfaea5200388ad8d70151b0ae3f92
Leonardo Raele <leonardoraele@gmail.com>
2025-04-28 16:35:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,129 @@
+const ONCE_SYMBOL = Symbol('once');
+const EMIT = Symbol('emit');
+const CLEAR = Symbol('clear');
+
+interface ListenerFunction {
+	(...args: any[]): unknown;
+	[ONCE_SYMBOL]?: boolean;
+}
+
+export interface BaseEventEmitterAPI {
+	[signalName: string|symbol]: ListenerFunction;
+}
+
+export interface SignalListenerOptions {
+	once?: boolean;
+	signal?: AbortSignal;
+}
+
+export class SignalEmitter<T extends BaseEventEmitterAPI> {
+	#listeners = new Map<keyof T, Set<BaseEventEmitterAPI['']>>();
+
+	[EMIT]<K extends keyof T>(signalName: K, ...args: Parameters<T[K]>): boolean {
+		const listeners = this.#listeners.get(signalName);
+		if (!listeners) {
+			return false;
+		}
+		for (const listener of listeners) {
+			if (listener[ONCE_SYMBOL]) {
+				this.off(signalName, listener);
+			}
+			try {
+				listener(...args);
+			} catch (e) {
+				console.error(e);
+			}
+		}
+		return true;
+	}
+
+	on<K extends keyof T>(signalName: K, listener: T[K]): void;
+	on<K extends keyof T>(signalName: K, options: SignalListenerOptions, listener: T[K]): void;
+	on<K extends keyof T>(signalName: K, ...args: any[]): void {
+		const listener: T[K] = typeof args[0] === 'function' ? args[0] : args[1];
+		const options: SignalListenerOptions|undefined = typeof args[0] === 'object' ? args[0] : undefined;
+		{
+			const listeners = this.#listeners.get(signalName);
+			if (listeners) {
+				listeners.add(listener);
+			} else {
+				this.#listeners.set(signalName, new Set([listener]));
+			}
+		}
+		{
+			if (options?.once) {
+				Object.defineProperty(listener, ONCE_SYMBOL, {
+					enumerable: false,
+					configurable: true,
+					writable: false,
+					value: true,
+				});
+			}
+		}
+		{
+			options?.signal?.addEventListener('abort', () => {
+				this.off(signalName, listener);
+			}, { once: true });
+		}
+	}
+
+	stream<K extends keyof T>(signalName: K): ReadableStream<Parameters<T[K]>> {
+		const aborter = new AbortController();
+		return new ReadableStream<Parameters<T[K]>>({
+			start: (controller) => {
+				this.on(signalName, { signal: aborter.signal }, ((...args: any[]) => {
+					controller.enqueue(args as any);
+				}) as any);
+			},
+			cancel: reason => aborter.abort(reason),
+		});
+	}
+
+	next<K extends keyof T>(signalName: K, { signal = undefined as AbortSignal|undefined } = {}): Promise<Parameters<T[K]>> {
+		return new Promise<Parameters<T[K]>>((resolve, reject) => {
+			signal?.addEventListener('abort', reason => reject(reason));
+			this.on(signalName, { once: true }, ((values: any) => resolve(values)) as any);
+		});
+	}
+
+	off(signalName: keyof T, listener: ListenerFunction): void {
+		delete listener[ONCE_SYMBOL];
+		const listeners = this.#listeners.get(signalName);
+		if (!listeners) {
+			return;
+		}
+		listeners.delete(listener);
+		if (listeners.size === 0) {
+			this.#listeners.delete(signalName);
+		}
+	}
+
+	[CLEAR](signalName?: keyof T): void {
+		if (signalName === undefined) {
+			Object.keys(this.#listeners).forEach(key => this[CLEAR](key));
+		} else {
+			this.#listeners.get(signalName)
+				?.forEach(listener => this.off(signalName, listener));
+		}
+	}
+}
+
+export class SignalController<T extends BaseEventEmitterAPI> {
+	readonly signal = new SignalEmitter<T>();
+
+	constructor(
+		// private readonly options: SignalControllerOptions = {},
+	) {}
+
+	emit<K extends keyof T>(signalName: K, ...args: Parameters<T[K]>): void {
+		this.signal[EMIT](signalName, ...args);
+	}
+
+	off(signalName: keyof T): void {
+		this.signal[CLEAR](signalName);
+	}
+
+	clear(): void {
+		this.signal[CLEAR]();
+	}
+}
